557ee7b91b7243164e235d874612a5e5a905f96e
Mahmoud <mahmoud.talass@gmail.com>
2023-06-14 23:09:58 -0400

    Add placeholder books to be generated on page

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let myLibrary = [];
 
+// Pre-made books
+
+myLibrary.push(new Book("The Odyssey", "Homer", 541, true));
+myLibrary.push(
+   new Book(
+      "Harry Potter and the Chamber of Secrets",
+      "J. K. Rowling",
+      341,
+      false
+   )
+);
+myLibrary.push(
+   new Book(
+      "A Dance with Dragons (A Song of Ice and Fire)",
+      "George R. R. Martin",
+      1152,
+      false
+   )
+);
+
 function Book(title, author, pages, read) {
    this.title = title;
    this.author = author;
@@ -15,5 +35,3 @@ function addBookToLibrary() {
 
    myLibrary.push(new Book(title, author, pages, read));
 }
-
-
